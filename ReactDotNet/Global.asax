cbfcfd959c3d885740d0a48ddfe613919cfc53f3
cciinnddyy0033 <cciinnddyy0033@gmail.com>
2021-06-27 15:54:33 +1000

    加入專案檔案。

---
+++ ReactDotNet/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactDotNet.MvcApplication" Language="C#" %>
---
+++ ReactDotNet/Models/Employees.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ReactDotNet.Models
+{
+    public class Employees
+    {
+        public Guid Id { get; set; }
+        public String FirstName{ get; set; }
+        public String SecondName { get; set; }
+    }
+}
